1a5fafd6748d44e9aa91317789099bfabc975f68
bym2922 <1824399664@qq.com>
2018-05-14 22:27:22 +0800

    3

---
+++ app/models.py
@@ -0,0 +1,14 @@
+from django.db import models
+
+# Create your models here.
+
+class Posts(models.Model):
+    title = models.CharField(max_length=50)
+    author = models.ForeignKey("Author",max_length=100,on_delete=models.CASCADE)
+    content = models.CharField(max_length=1500)
+    # summary = models.CharField(max_length=300)
+
+
+class Author(models.Model):
+    name = models.CharField(max_length=30)
+    art_nums = models.IntegerField()
